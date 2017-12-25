435ea9b56af408bde9cc09bbf248242bc98c748c
Your Name <you@example.com>
2017-12-25 12:21:26 +0800

    add slf4j logger

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GreetingController {
+	
+	private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -23,7 +27,7 @@ public class GreetingController {
 //		Greeting data = restTemplate.getForObject("http://www.opptty.com:8084/greeting", Greeting.class);
 		Greeting data = restTemplate.getForObject("http://10.129.0.12:8080/greeting",Greeting.class);
 		data.setContent("message got:" + data.getContent());
-		System.out.print("print info:" + data.getContent());
+		log.info("print info:" + data.getContent());
 		return data;
 	}
 
